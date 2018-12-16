a3b434b90562113aacb363be897f4edb9aa5dc1d
Sedhumani <Sedhumani005@gmail.com>
2018-12-16 18:13:40 +0300

    src & static: Game modules and packages

---
+++ src/Constants.ts
@@ -0,0 +1,27 @@
+exports.default = {
+    WIDTH: 12,
+    HEIGHT: 24,
+    SQUARE_SIZE: 25,
+    COLORS: {
+        TETRISBLOCK_BORDERS: '#373c40',
+        TETRISBLOCK_I: '#ff8000',
+        TETRISBLOCK_J: '#2cc990',
+        TETRISBLOCK_L: '#f34344',
+        TETRISBLOCK_O: '#ffdf00',
+        TETRISBLOCK_S: '#ccdce4',
+        TETRISBLOCK_T: '#008aff',
+        TETRISBLOCK_Z: '#fcb941',
+        BACKGROUND: '#2d3236',
+        BORDERS: '#373C40'
+    },
+    DOM: {
+        CONTAINER: '#canvas-container',
+        NEXT: '#next-tetrisblock',
+        START_PAUSE: '#start-pause button',
+        LEVEL: '#level',
+        SCORE: '#score',
+        CLEARED: '#cleared',
+        BEST: '#best-score',
+        OVERLAY: '#overlay'
+    }
+};
--- src/Games.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Game = /** @class */ (function () {
-    function Game() {
-    }
-    return Game;
-}());
-exports.default = Game;
